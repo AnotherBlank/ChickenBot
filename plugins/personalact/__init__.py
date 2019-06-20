10677a9ef8d5ec4aa71fc5ba7a56f2346da11411
AnotherBlank <jp5715153@yeah.net>
2019-06-21 03:25:10 +0800

    删除了一个词条。

--- plugins/personalact/__init__.py
+++ plugins/personalact/__init__.py
@@ -1,15 +1,18 @@
 # 这是对话内容
-from nonebot import on_natural_language, CommandSession
+
+from jieba import posseg
+
+from nonebot import on_natural_language, CommandSession, NLPSession
 
 
 @on_natural_language(keywords={'嫁给我', '嫁我', '娶你', '结婚'})
-async def _(session: CommandSession):
+async def _(session: NLPSession):
     await session.send('绝！对！不！行！')
 
 
-@on_natural_language(keywords={'你是谁', '是谁', '谁'})
-async def _(session: CommandSession): await session.send('我可是超绝可爱的十一姬。')
+@on_natural_language(keywords={'你是谁', '谁'})
+async def _(session: NLPSession): await session.send('我可是超绝可爱的十一姬。')
 
 
 @on_natural_language(keywords={'主人是谁', '谁是你的主人'})
-async def _(session: CommandSession): await session.send('是十三姬前辈。')
+async def _(session: NLPSession): await session.send('是十三姬前辈。')
