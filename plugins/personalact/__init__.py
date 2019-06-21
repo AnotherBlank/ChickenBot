3238092b1692d3f6e5a2a7c597236a328cca32c5
AnotherBlank <jp5715153@yeah.net>
2019-06-22 01:55:54 +0800

    群聊好像总是无脑触发，就先改成私聊了

--- plugins/personalact/__init__.py
+++ plugins/personalact/__init__.py
@@ -1,28 +1,23 @@
 # 这是对话内容
 from typing import Optional
 
-from jieba import posseg
-
 from nonebot import *
-from nonebot import logger
 from nonebot.helpers import render_expression
 from nonebot.permission import PRIVATE, GROUP
-from nonebot.command.argfilter import validators, extractors, ValidateError
 import random
 
 from util.SqlHelper import SqlHelper
 
-# 无法回答指令时候的表达
 EXPR_DONT_UNDERSTAND = (
-    'emmmmmm……你在说什么？',
     '??????',
-    '听！不！懂！',
-    '这条信息可能超出了我的理解范围',
-    '跟机器人对话要用机器人语！用help来查！',
-    'ERROR: 我没听懂'
-    '(。_。)糟糕，触及到了我的知识盲区...',
+    '……你想表达什么？'
+    '你得明白你对面是机器人，请用机器人的语言来说！'
+    '……人类的语言好深奥'
+    'ERROR: 无法理解'
+    '虽然不明白你说的意思，但这个时候卖萌就对了！(<ゝω·)☆~'
+    '我！听！不！懂！'
+    '……我需要一个人来教教我怎么回答这句'
 )
-
 
 @on_command('study', permission=GROUP, only_to_me=False)
 async def study(session: CommandSession):
@@ -50,8 +45,9 @@ async def _(session: CommandSession):
     # 如果不需要对参数进行特殊处理，则不用再手动加入 state，NoneBot 会自动放进去
 
 
-@on_natural_language(permission=PRIVATE)
+@on_natural_language(permission=PRIVATE, only_to_me=False)
 async def _(session: NLPSession):
+    # return IntentCommand(60, 'justspeak', args={'message': session.msg_text})
     return IntentCommand(60, 'justspeak', args={'message': session.msg_text})
 
 
@@ -60,14 +56,14 @@ async def justSpeak(session: CommandSession):
     # 获取可选参数，这里如果没有 message 参数，命令不会被中断，message 变量会是 None
     message = session.state.get('message')
 
-    # 通过封装的函数获取图灵机器人的回复
-    reply = await call_response(message)
+    # 获取回复
+    reply = await call_response(str(message))
     if reply:
-
         await session.send(reply)
     else:
         # 这里的 render_expression() 函数会将一个「表达」渲染成一个字符串消息
         await session.send(render_expression(EXPR_DONT_UNDERSTAND))
+        # return
 
 
 # 回复的api方法，返回一个字符串
