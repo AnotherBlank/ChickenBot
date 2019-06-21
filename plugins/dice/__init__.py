bca493dafb49cb62b194f303455b085fe1f4ac93
AnotherBlank <jp5715153@yeah.net>
2019-06-22 03:53:03 +0800

    增加骰子

--- plugins/dice/__init__.py
+++ plugins/dice/__init__.py
@@ -1,40 +1,26 @@
-# from attr import validators
-# from nonebot import on_command, CommandSession
-# from nonebot.command.argfilter import extractors
-# from nonebot.permission import PRIVATE
-#
-# from plugins.personalact import my_custom_validator
-#
-#
-# @on_command('demo', permission=PRIVATE, only_to_me=False)
-# async def demo(session: CommandSession):
-#     question = session.state.get('arg1_derived')  # 从会话状态里尝试获取
-#     if question is None:
-#         arg1: int = session.get(
-#             'arg1',
-#             prompt='请输入参数1',
-#             arg_filters=[
-#                 extractors.extract_text,  # 提取纯文本部分
-#                 str.strip,  # 去掉两边的空白
-#                 validators.not_empty(),
-#                 validators.match_regex(r'[0-9a-zA-Z]{6,20}', '必须为6~20位字符'),
-#                 my_custom_validator,  # 自定义验证器
-#                 int,  # 转换成 int
-#                 validators.ensure_true(lambda x: x > 20000000, '必须大于2000000')
-#             ],
-#             at_sender=True,
-#         )
-#         question = arg1 + 42
-#         session.state['arg1_derived'] = question  # 修改会话状态
-#
-#     arg3 = session.get('arg3', prompt='你的arg3是什么呢？')
-#
-#     reply = f'arg1_derived:\n{question}\narg3:\n{arg3}'
-#     session.finish(reply)
-#
-#
-# @demo.args_parser
-# async def _(session: CommandSession):
-#     if session.is_first_run and session.current_arg_text.strip():
-#         # 第一次运行，如果有参数，则设置给 arg3
-#         session.state['arg3'] = session.current_arg_text.strip()
+from nonebot import on_command, CommandSession,logger
+from nonebot.permission import GROUP, PRIVATE
+import random
+
+
+@on_command('roll', permission=GROUP | PRIVATE, only_to_me=False)
+async def roll(session: CommandSession):
+    data = session.state.get('data')  # 属于为xdy的形式
+    nickname = session.ctx['sender']['nickname']
+    randomresult = 0
+    if data is None:  # 默认就1d100
+        randomresult = random.randint(1, 100)
+        await session.finish(nickname + '投掷出: ' + str(randomresult))
+    else:
+        datas = str(data).split('d')
+        i = 0
+        while i < int(datas[0]):
+            randomresult += random.randint(1, int(datas[1]))
+            i += 1
+        await session.finish(nickname + '掷' + datas[0] + '次' + datas[1] + '面骰的总点: ' + str(randomresult))
+
+
+@roll.args_parser
+async def _(session: CommandSession):
+    if session.is_first_run and session.current_arg_text.strip():
+        session.state['data'] = session.current_arg_text.strip()
--- plugins/helper/__init__.py
+++ plugins/helper/__init__.py
@@ -14,6 +14,7 @@ async def help(session: CommandSession):
                        'azlightten——舰B轻型建建造\nazheavy | azheavyten——舰b重型舰建造\nazspecial | '
                        'azspecialten——舰b特型舰建造\njrrp——人品\ndraw——抽签\nhelp——帮助\nwhoami '
                        '——我是谁\nsleep——禁言礼包\nsign | 签到——签到功能 签到后可享受精致睡眠在线记录 (被选中的人会出现BUG)\nsleeprank——精致睡眠排行榜\n'
+                       'roll——投掷骰子，支持xdy式骰子(/roll 1d20),默认为1d100\n'
                        'study——语言学习指令，后面可以接一个问题的参数(/study 问题),私聊专属')
 
 
